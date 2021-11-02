5133aca6bc96fa1789f585ea0ae3480c02831446
kart6302 <93569908+kart6302@users.noreply.github.com>
2021-11-02 11:53:08 +0530

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main ()
+{
+	int a,b,s,d,p,q,r;
+	a = 11;
+	b = 2;
+	s = a+b;
+	d = a-b;
+	p = a*b;
+	q = a/b; 
+	r = a%b;
+	printf("sum of %d and %d is %d:\n",a,b,s);
+	
+	printf ("difference of %d and %d is %d:\n",a,b,d);
+	
+	printf ("ptoduct of %d and %d is %d:\n",a,b,p);
+	
+	printf ("quotient of %d and %d is %d:\n",a,b,q);
+	
+	printf ("remainder of %d and %d is %d :\n",a,b,r);
+}
