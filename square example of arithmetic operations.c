3ce9987663f79bed0d79b54abdb63e0815899920
kart6302 <93569908+kart6302@users.noreply.github.com>
2021-11-03 11:29:37 +0530

    Add files via upload

---
+++ square example of arithmetic operations.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int s,a,p;
+	
+	s=8;
+	
+	a=s*s;
+	
+	p=4*s;
+	
+	printf ("     area of the square with side %d is %d:\n",s,a);
+	printf("perimeter of the square with side %d is %d:",s,p);
+}
