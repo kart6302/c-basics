b1998a78b4de887104d159349d6625c6775bdb8f
kart6302 <93569908+kart6302@users.noreply.github.com>
2021-11-09 12:26:42 +0530

    Add files via upload

---
+++ ASCII char to num.c
@@ -0,0 +1,8 @@
+
+void main()
+{
+	char c;
+	printf ("enter a charecter (a-z or A-Z)");
+	scanf ("%c",&c);
+	printf ("corresponding ASCII number for %c is %d", c, c);
+}
---
+++ ASCII num to char.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf ("enter a number (65-90 or 97-122)");
+	scanf ("%d",&n);
+	printf ("corresponding ASCII charecter for %d is %c", n, n);
+}
