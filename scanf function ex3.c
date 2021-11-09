061e10656be8da50a71e4a2c04fa51b0a46f7cef
kart6302 <93569908+kart6302@users.noreply.github.com>
2021-11-09 12:02:18 +0530

    Add files via upload

---
+++ scanf function ex3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main ()
+{
+
+	int a,b,c,d;
+	printf ("enter values of a,b,c&d");
+	scanf ("%d%d%d%d", &a, &b, &c, &d);
+	printf ("product of %d,%d,%d&%d is %d ",a,b,c,d,a*b*c*d);
+		
+} 
---
+++ using scanf function 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf ("enter three number");
+	scanf ("%d%d%d",&a,&b,&c);
+	printf ("number 1: %d\n",a);
+	printf ("number 2: %d\n",b);
+	printf ("number 3: %d\n",c);
+}
